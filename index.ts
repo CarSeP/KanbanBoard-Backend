37dec79db7d5730749a0b90b95ceac1620ba0227
Carsep <vjpd64@gmail.com>
2025-10-13 22:25:29 -0400

    Implement database connection verification on app startup

--- index.ts
+++ index.ts
@@ -1,7 +1,14 @@
-import app from "./src/main"
+import { verifyDatabaseConnection } from "./src/services/prisma.service";
+import app from "./src/main";
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-  console.log(`App listening on port ${port}`)
-})
+app.listen(port, async () => {
+  console.log(`App listening on port ${port}`);
+
+  if (await verifyDatabaseConnection()) {
+    console.log("Connection to the database established successfully");
+  } else {
+    console.log("Error connecting to the database");
+  }
+});
--- src/services/prisma.service.ts
+++ src/services/prisma.service.ts
@@ -1,3 +1,12 @@
 import { PrismaClient } from "./../../generated/prisma";
 
 export const prisma = new PrismaClient();
+
+export const verifyDatabaseConnection = async () => {
+  try {
+    await prisma.$connect();
+    return true;
+  } catch {
+    return false;
+  }
+};
