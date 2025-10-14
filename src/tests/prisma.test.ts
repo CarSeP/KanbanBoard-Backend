32c1fdebb63b9619f87368e7c8d7e7de31600623
Carsep <vjpd64@gmail.com>
2025-10-13 22:25:53 -0400

    Create prisma test

---
+++ src/tests/prisma.test.ts
@@ -0,0 +1,5 @@
+import { verifyDatabaseConnection } from "./../services/prisma.service";
+
+test("verify connection to the database", async () => {
+  expect(await verifyDatabaseConnection()).toBe(true);
+});
