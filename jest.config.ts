6efa1386179ad1c8ca24d09af65ec6616966c09f
Carsep <vjpd64@gmail.com>
2025-10-13 22:25:04 -0400

    Add jest.config.ts

---
+++ jest.config.ts
@@ -0,0 +1,15 @@
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.d.ts',
+    '!src/**/*.test.ts',
+    '!src/**/*.spec.ts'
+  ],
+  coverageDirectory: 'coverage',
+  verbose: true
+};
