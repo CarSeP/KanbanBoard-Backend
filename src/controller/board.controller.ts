93148624801b2da8bc6bd174988ce8b07aa70397
Carsep <vjpd64@gmail.com>
2025-10-14 21:57:18 -0400

    Implement board controller and refactor router to use controller methods

---
+++ src/controller/board.controller.ts
@@ -0,0 +1,102 @@
+import { Request, Response } from "express";
+import {
+  deleteBoard,
+  getAllBoards,
+  getUniqueBoard,
+  upsertBoard,
+  validateBoard,
+} from "@services/board.service";
+
+const getAll = async (req: Request, res: Response) => {
+  try {
+    const [boards, totalCount] = await getAllBoards();
+
+    return res.status(200).json({
+      success: true,
+      totalCount,
+      boards,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: ["Server error"],
+    });
+  }
+};
+
+const getOne = async (req: Request, res: Response) => {
+  try {
+    const id = req.params.id;
+    const board = await getUniqueBoard(id);
+
+    if (!board)
+      return res.status(404).json({
+        success: false,
+        message: ["Board not found"],
+      });
+
+    return res.status(200).json({
+      success: true,
+      board,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: ["Server error"],
+    });
+  }
+};
+
+const deleteOne = async (req: Request, res: Response) => {
+  try {
+    const id = req.params.id;
+    const board = await deleteBoard(id);
+
+    if (!board)
+      return res.status(404).json({
+        success: false,
+        message: ["Board not found"],
+      });
+
+    return res.status(200).json({
+      success: true,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: ["Server error"],
+    });
+  }
+};
+
+const upsertOne = async (req: Request, res: Response) => {
+  try {
+    const body = req.body;
+    const validate = validateBoard(body);
+
+    if (!validate.validate) {
+      return res.status(400).json({
+        success: false,
+        message: validate.errors,
+      });
+    }
+
+    await upsertBoard(body);
+
+    return res.status(200).json({
+      success: true,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: ["Server error"],
+    });
+  }
+};
+
+export const boardController = {
+  getAll,
+  getOne,
+  deleteOne,
+  upsertOne,
+};
--- src/routers/board.router.ts
+++ src/routers/board.router.ts
@@ -1,69 +1,9 @@
+import { boardController } from "controller/board.controller";
 import { Router } from "express";
-import {
-  deleteBoard,
-  getAllBoards,
-  getUniqueBoard,
-  upsertBoard,
-  validateBoard,
-} from "@services/board.service";
 
 export const boardRouter = Router();
 
-boardRouter.get("/", async (req, res) => {
-  const [boards, totalCount] = await getAllBoards();
-
-  return res.status(200).json({
-    success: true,
-    totalCount,
-    boards,
-  });
-});
-
-boardRouter.get("/:id", async (req, res) => {
-  const id = req.params.id;
-  const board = await getUniqueBoard(id);
-
-  if (!board)
-    return res.status(404).json({
-      success: false,
-      message: ["Board not found"],
-    });
-
-  return res.status(200).json({
-    success: true,
-    board,
-  });
-});
-
-boardRouter.delete("/:id", async (req, res) => {
-  const id = req.params.id;
-  const board = await deleteBoard(id);
-
-  if (!board)
-    return res.status(404).json({
-      success: false,
-      message: ["Board not found"],
-    });
-
-  return res.status(200).json({
-    success: true,
-  });
-});
-
-boardRouter.put("/", async (req, res) => {
-  const body = req.body;
-  const validate = validateBoard(body);
-
-  if (!validate.validate) {
-    return res.status(400).json({
-      success: false,
-      message: validate.errors,
-    });
-  }
-
-  await upsertBoard(body);
-
-  return res.status(200).json({
-    success: true,
-  });
-});
+boardRouter.get("/", boardController.getAll);
+boardRouter.put("/", boardController.upsertOne);
+boardRouter.get("/:id", boardController.getOne);
+boardRouter.delete("/:id", boardController.deleteOne);
