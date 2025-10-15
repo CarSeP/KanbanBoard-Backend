6b71385d223c9ab8a2d899911a9f5d46bfc02943
Carsep <vjpd64@gmail.com>
2025-10-14 22:28:03 -0400

    Use pino

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import express from "express";
 import { boardRouter } from "@routers/board.router";
 import swaggerUI from "swagger-ui-express";
-import swaggerDocument from "./../swagger.json"
+import swaggerDocument from "./../swagger.json";
+import pino from "pino-http";
 
 const app = express();
 
 app.use(express.json());
+app.use(pino());
+
 app.use("/board", boardRouter);
-app.use("/swagger", swaggerUI.serve, swaggerUI.setup(swaggerDocument))
+app.use("/swagger", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 export default app;
