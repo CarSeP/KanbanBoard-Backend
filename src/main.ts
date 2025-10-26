859372c04902b6224e6d8cf7573e75d581504c43
Carsep <vjpd64@gmail.com>
2025-10-26 10:35:29 -0400

    Use cors

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import pino from "pino-http";
+import cors from "cors";
 import { boardRouter } from "@routers/board.router";
 import { swaggerRouter } from "@routers/swagger.router";
 import { notFoundRouter } from "@routers/notFound.router";
@@ -10,6 +11,7 @@ const app = express();
 
 app.use(express.json());
 app.use(pino());
+app.use(cors());
 
 app.use("/swagger", swaggerRouter);
 app.use("/board", boardRouter);
