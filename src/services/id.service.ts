25f421ddaaff618306126e54c9a836d546a12f8a
Carsep <vjpd64@gmail.com>
2025-10-28 22:09:49 -0400

    Create id.service.ts

---
+++ src/services/id.service.ts
@@ -0,0 +1,10 @@
+export const generateId = (length: number) => {
+  const characters =
+    "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890";
+  let id = "";
+  for (let i = 0; i < length; i++) {
+    const random = Math.floor(Math.random() * characters.length);
+    id += characters[random];
+  }
+  return id;
+};
