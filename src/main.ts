40c43f0361e7f9cb28bc962edf197aab4b83eaea
CarSeP <vjpd64@gmail.com>
2026-05-13 12:29:08 -0400

    feat: add pino-pretty for enhanced logging and create pino service configuration

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import express from "express";
-import pino from "pino-http";
 import cors from "cors";
+import { getPinoConfig } from "@services/pino.service";
 import { getCors } from "@services/cors.service";
 import { authMiddleware } from "@middlewares/auth.middleware";
 import { boardRouter } from "@routers/board.router";
@@ -13,7 +13,7 @@ import { authRouter } from "@routers/auth.router";
 const app = express();
 
 app.use(express.json());
-app.use(pino());
+app.use(getPinoConfig());
 app.use(cors(getCors()));
 app.use("/swagger", swaggerRouter);
 app.use("/auth", authRouter);
